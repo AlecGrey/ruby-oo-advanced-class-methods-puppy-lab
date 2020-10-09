9397c5fd5c7efb4befe8867dcdfef54276cb3ee2
AlecGrey <alecgrey.coding@gmail.com>
2020-10-09 10:25:56 -0700

    completed lab

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,35 @@
 # Add your code here
+require 'pry'
+
+class Dog
+
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        self.save
+    end
+
+    # ~~ class methods ~~ #
+
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+        @@all = []
+    end
+
+    def self.print_all
+        @@all.each {|dog| puts dog.name}
+    end
+
+    # ~~ instance methods ~~ #
+
+    def save
+        @@all << self
+    end
+
+end
